c992edf1115d9f1520f2aebcdce02e84a79a0934
Anime-Quatroctus <kanadetachibona@gmail.com>
2021-06-06 22:33:33 -0700

    Intended flake8 error.

--- calculator.py
+++ calculator.py
@@ -6,14 +6,11 @@ Calculator app using GitHub Actions.
 def add(x, y):
     return x + y
 
-
 def subtract(x, y):
     return x - y
 
-
 def multiply(x, y):
     return x * y
-
 
 def divide(x, y):
     return x / y
